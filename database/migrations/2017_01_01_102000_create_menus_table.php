adbb90bfef592f77ca941217111d25b548658dc9
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2023-10-02 23:55:48 +0000

    resolve primary key data type

--- database/migrations/2017_01_01_102000_create_menus_table.php
+++ database/migrations/2017_01_01_102000_create_menus_table.php
@@ -9,12 +9,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('menus', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
 
-            $table->unsignedInteger('parent_id')->index()->nullable();
+            $table->unsignedBigInteger('parent_id')->index()->nullable();
             $table->foreign('parent_id')->references('id')->on('menus');
 
-            $table->integer('permission_id')->unsigned()->index()->nullable();
+            $table->unsignedBigInteger('permission_id')->index()->nullable();
             $table->foreign('permission_id')->references('id')->on('permissions');
 
             $table->string('name');
